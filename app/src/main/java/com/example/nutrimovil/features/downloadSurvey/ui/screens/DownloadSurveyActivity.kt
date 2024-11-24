f1ea72b812c02dab20346aaad826b7003e37dbf2
PedroPollo <72528531+PedroPollo@users.noreply.github.com>
2024-11-23 20:10:40 -0600

    Carga de las encuestas en DownloadActivity

--- app/src/main/java/com/example/nutrimovil/features/downloadSurvey/ui/screens/DownloadSurveyActivity.kt
+++ app/src/main/java/com/example/nutrimovil/features/downloadSurvey/ui/screens/DownloadSurveyActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -27,6 +29,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -51,10 +54,10 @@ class DownloadSurveyActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             NutriMovilTheme {
+                downloadSurveyViewModel.getSurveysFromCloud(us!!.researchers)
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = Fondo
                 ) {
-                    downloadSurveyViewModel.getSurveysFromCloud(us!!.researchers)
                     DownloadableSurveys(context = this, id = us.id)
                 }
             }
@@ -70,80 +73,107 @@ fun DownloadableSurveys(
     downloadSurveyViewModel: DownloadSurveyViewModel = viewModel(),
     id: String
 ) {
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = SecundarioVar
-                ),
-                title = { Text(text = "Encuestas para descargar", fontFamily = FontFamily.Monospace) },
-                navigationIcon = {
-                    IconButton(onClick = { context.finish() }) {
-                        Icon(Icons.Filled.ArrowBack, "Regresar")
+    val surveys by downloadSurveyViewModel.surveys.collectAsState() // Observa el estado
+
+    if (surveys == null) {
+        // Mostrar indicador de carga mientras los datos se están obteniendo
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = SecundarioVar
+                    ),
+                    title = {
+                        Text(
+                            text = "Encuestas para descargar",
+                            fontFamily = FontFamily.Monospace
+                        )
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { context.finish() }) {
+                            Icon(Icons.Filled.ArrowBack, "Regresar")
+                        }
+                    }
+                )
+            }
+        ) {
+            var selectedSurvey: Survey? by remember { mutableStateOf(null) } // Para almacenar la encuesta seleccionada
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                var isExpanded by remember {
+                    mutableStateOf(false)
+                }
+                var selectedItem by remember {
+                    mutableStateOf("")
+                }
+                Text("Seleccione una encuesta para descargar")
+                ExposedDropdownMenuBox(
+                    expanded = isExpanded,
+                    onExpandedChange = { isExpanded = it }) {
+                    TextField(
+                        value = selectedItem,
+                        onValueChange = {},
+                        readOnly = true,
+                        trailingIcon = {
+                            ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                        },
+                        colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                        modifier = Modifier.menuAnchor()
+                    )
+                    ExposedDropdownMenu(
+                        expanded = isExpanded,
+                        onDismissRequest = { isExpanded = false }
+                    ) {
+                        if (surveys != null) {
+                            for (label in surveys!!) {
+                                DropdownMenuItem(
+                                    text = { Text(text = label.nombre ?: "") },
+                                    onClick = {
+                                        selectedItem = label.nombre ?: ""
+                                        isExpanded = false
+                                        selectedSurvey = label
+                                    })
+                            }
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "check your internet connection",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
                     }
                 }
-            )
-        }
-    ) {
-        val lista = downloadSurveyViewModel.surveys
-        var selectedSurvey: Survey? by remember { mutableStateOf(null) } // Para almacenar la encuesta seleccionada
-        Column(
-            modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            var isExpanded by remember {
-                mutableStateOf(false)
-            }
-            var selectedItem by remember {
-                mutableStateOf("")
-            }
-            Text("Seleccione una encuesta para descargar")
-            ExposedDropdownMenuBox(
-                expanded = isExpanded,
-                onExpandedChange = { isExpanded = it }) {
-                TextField(
-                    value = selectedItem,
-                    onValueChange = {},
-                    readOnly = true,
-                    trailingIcon = {
-                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
-                    },
-                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                    modifier = Modifier.menuAnchor()
-                )
-                ExposedDropdownMenu(
-                    expanded = isExpanded,
-                    onDismissRequest = { isExpanded = false }
-                ) {
-                    if (lista != null) {
-                        for (label in lista) {
-                            DropdownMenuItem(text = { Text(text = label?.nombre ?: "") }, onClick = {
-                                selectedItem = label?.nombre ?: ""
-                                isExpanded = false
-                                selectedSurvey = label
-                            })
-                        }
-                    } else {
-                        Toast.makeText(
+                Button(
+                    modifier = Modifier.padding(bottom = 30.dp),
+                    colors = ButtonDefaults.buttonColors(PrimarioVar),
+                    onClick = {
+                        selectedSurvey?.let { survey ->
+                            downloadSurveyViewModel.downloadSurvey(
+                                context = context,
+                                survey = survey,
+                                id = id
+                            )
+                            Toast.makeText(context, "Encuesta descargada", Toast.LENGTH_SHORT)
+                                .show()
+                            context.finish()
+                        } ?: Toast.makeText(
                             context,
-                            "check your internet connection",
+                            "Seleccione una encuesta primero",
                             Toast.LENGTH_SHORT
                         ).show()
-                    }
+                    }) {
+                    Text("Descargar encuesta", fontFamily = FontFamily.Monospace)
                 }
-            }
-            Button(
-                modifier = Modifier.padding(bottom = 30.dp),
-                colors = ButtonDefaults.buttonColors(PrimarioVar),
-                onClick = {
-                    selectedSurvey?.let { survey ->
-                        downloadSurveyViewModel.downloadSurvey(context = context, survey = survey, id = id)
-                        Toast.makeText(context, "Encuesta descargada", Toast.LENGTH_SHORT).show()
-                        context.finish()
-                    } ?: Toast.makeText(context, "Seleccione una encuesta primero", Toast.LENGTH_SHORT).show()
-            }) {
-                Text("Descargar encuesta", fontFamily = FontFamily.Monospace)
             }
         }
     }
